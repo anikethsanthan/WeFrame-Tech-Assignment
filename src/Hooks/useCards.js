5da731c8356dd6905a1500c0bdc19dd35a3db413
anikethsanthan <anikethsanthan999@gmail.com>
2024-12-09 20:32:19 +0530

    made card items dynamic and made it congif griven

--- src/Hooks/useCards.js
+++ src/Hooks/useCards.js
@@ -1,36 +1,42 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React from 'react'
+import React from 'react';
+ import { cardData } from '@/utilities/utils.js';
 
 const useCards = () => {
   return (
-    <div>
-        <div className='w-[330px] h-[438px] mr-[12px]  rounded-[8px] '>
+    <div className='flex'>
+        {cardData.map((card, index) => (<>
+        <div >
+       
+        <div key={index} className='w-[330px] h-[438px] mr-[24px]  rounded-[8px]  '>
                     {/* like section */}
-                    <div className='absolute top-[127px] flex justify-between'>
+                    <div  className='absolute top-[127px] flex justify-between'>
                         <div>
                         <img  className="h-[32px] w-[32px] hover:shadow-lg ml-[12px]" src='./Vector.heartpng.png' alt='heart'></img>
                         </div>
                         <div className='bg-[#FFFFFF] w-[100px] h-[18px] rounded-[4px] ml-[166px]'>
-                        <p className='font-medium text-[10px] text-[#111928] text-center pt-[2px]   '>Art de la table</p>
+                        <p className='font-medium text-[10px] text-[#111928] text-center pt-[2px]   '>{card.smallTitle}</p>
                         </div>   
                     </div>
                     {/* main image */}
                     <div className='bg-[#F9F7F5]'>
-                    <img  className="w-[330px] h-[362px] object-contain " src='./Table ronde 165cm Festi 1.png' alt='itemimg'></img>
+                    <img  className="w-[330px] h-[362px] object-contain " src={card.imageurl} alt='itemimg'></img>
                     </div>
                     {/* heading and main price tag */}
                     <div className='flex justify-between w-[310px] pl-[10px]'>
-                        <h1 className='text-[24px] font-normal'>Title</h1>
-                        <p className='text-[24px] font-medium'>0<sup>€</sup></p>
+                        <h1 className='text-[24px] font-normal'>{card.title}</h1>
+                        <p className='text-[24px] font-medium'>{card.price}<sup>€</sup></p>
                     </div>
                     {/* extra info */}
                     <div className='flex justify-between w-[310px] pl-[10px] mt-2'>
-                        <h1 className='font-normal text-[14px] text-[#9C9C9C]'>o.35€/Piece .REF.VABGN5 </h1>
-                        <p className='bg-[#F1F4F6] w-[68px] h-[26px] text-[11px] font-medium text-center pt-[5px] text-[#546A7D] rounded-lg'>20 Pieces</p>
+                        <h1 className='font-normal text-[14px] text-[#9C9C9C]'>{card.fullPrice}€/Piece .REF.{card.ref} </h1>
+                        <p className='bg-[#F1F4F6] w-[68px] h-[26px] text-[11px] font-medium text-center pt-[5px] text-[#546A7D] rounded-lg'>{card.pieces} Pieces</p>
                     </div>
 
                 </div>
+                </div>
+                </>))}
       
     </div>
   )
--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,7 +1,7 @@
 export const cardData = [
     {
-      "image-url": "./Card Header.png",
-      title: "Title 1",
+      imageurl: "./Table ronde 165cm Festi 1.png",
+      title: "Title ",
       smallTitle: "Art de la table",
       price: "0",
       pieces: 20,
@@ -9,13 +9,41 @@ export const cardData = [
       fullPrice: 0.35,
     },
     {
-      "image-url": "./Another Image.png",
-      title: "Title 2",
-      smallTitle: "Furniture",
-      price: "10",
-      pieces: 15,
-      ref: "XYZ123",
-      fullPrice: 1.5,
+        imageurl: "./Table ronde 165cm Festi 1.png",
+        title: "Title 1",
+        smallTitle: "Art de la table",
+        price: "0",
+        pieces: 20,
+        ref: "VABGN5",
+        fullPrice: 0.35,
     },
+    {
+        imageurl: "./Table ronde 165cm Festi 1.png",
+        title: "Title 1",
+        smallTitle: "Art de la table",
+        price: "0",
+        pieces: 20,
+        ref: "VABGN5",
+        fullPrice: 0.35,
+    },
+    {
+        imageurl: "./Table ronde 165cm Festi 1.png",
+        title: "Title 1",
+        smallTitle: "Art de la table",
+        price: "0",
+        pieces: 20,
+        ref: "VABGN5",
+        fullPrice: 0.35,
+    },
+    {
+        imageurl: "./Table ronde 165cm Festi 1.png",
+        title: "Title 1",
+        smallTitle: "Art de la table",
+        price: "0",
+        pieces: 20,
+        ref: "VABGN5",
+        fullPrice: 0.35,
+    },
+
   ];
   
